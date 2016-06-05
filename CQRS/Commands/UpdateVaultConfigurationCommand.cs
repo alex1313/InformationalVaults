46bc20896b2cd6437817993564c2245f9314dce6
alex <sav.1209@gmail.com>
2016-06-05 18:41:03 +0500

    fixed stub update vault command

--- CQRS/Commands/UpdateVaultConfigurationCommand.cs
+++ CQRS/Commands/UpdateVaultConfigurationCommand.cs
@@ -6,7 +6,21 @@
     {
         public override void Execute(UpdateVaultConfigurationContext context)
         {
-            
+            var configuration = context.Configuration;
+
+            using (var uow = UnitOfWorkFactory.Create())
+            {
+                var vault = uow.VaultRepository.GetById(configuration.Id);
+
+                vault.Name = configuration.Name;
+                vault.Description = configuration.Description;
+                vault.OpenTime = configuration.OpenTime;
+                vault.CloseTime = configuration.CloseTime;
+
+                uow.VaultRepository.Update(vault);
+
+                uow.Commit();
+            }
         }
     }
 }
